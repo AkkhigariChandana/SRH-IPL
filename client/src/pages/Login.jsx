48e22143fa887ab3a9a94902e2a4f3cdf7dd8a03
secretrog <akk013192@gmail.com>
2026-05-28 15:17:27 +0530

    Add dynamic background image to login page with premium glassmorphism effect

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import Navbar from "../components/Navbar";
+import loginBg from "../assets/login-bg.jpg";
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -43,24 +44,45 @@ const handleSubmit = async () => {
   return (
     <div
       style={{
-        background: "#000",
+        position: "relative",
         minHeight: "100vh",
         color: "white",
+        backgroundColor: "#000",
       }}
     >
-      <Navbar />
-
+      {/* BACKGROUND IMAGE OVERLAY */}
       <div
         style={{
-          maxWidth: "500px",
-          margin: "100px auto",
-          padding: "40px",
-          background:
-            "linear-gradient(to bottom, #1a0000, #000)",
-          borderRadius: "25px",
-          border: "2px solid #ff6600",
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          backgroundImage: `url(${loginBg})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          opacity: 0.35,
+          zIndex: 0,
         }}
-      >
+      ></div>
+
+      <div style={{ position: "relative", zIndex: 1 }}>
+        <Navbar />
+
+        <div
+          style={{
+            maxWidth: "500px",
+            margin: "100px auto",
+            padding: "40px",
+            background:
+              "linear-gradient(to bottom, rgba(26,0,0,0.85), rgba(0,0,0,0.95))",
+            borderRadius: "25px",
+            border: "2px solid #ff6600",
+            boxShadow: "0 0 40px rgba(255,102,0,0.5)",
+            backdropFilter: "blur(5px)",
+          }}
+        >
         <h1
           style={{
             textAlign: "center",
@@ -93,6 +115,7 @@ const handleSubmit = async () => {
         >
           Login
         </button>
+      </div>
       </div>
     </div>
   );
